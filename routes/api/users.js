ccde4f02b054c0375ceee5fac1193e751cf80733
Chad Towle <chad@towlebooth.com>
2019-01-24 19:44:50 -0600

    Login and register validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,11 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
+// Load User model
 const User = require('../../models/User');
 
 // @route GET api/users/test
@@ -18,6 +23,13 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"})
 // @desc Register user
 // @access Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // check validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
         .then(user => {
             if(user) {
@@ -53,6 +65,13 @@ router.post('/register', (req, res) => {
 // @desc Login user / retruning JWT token
 // @access Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // check validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -61,7 +80,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             // check for user
             if(!user) {
-                return res.status(404).json({email: 'User not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             // check password
@@ -84,7 +104,8 @@ router.post('/login', (req, res) => {
                             });
                         });
                     } else {
-                        return res.status(400).json({password: 'Password incorrect'});
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
                     }
                 })
         })
