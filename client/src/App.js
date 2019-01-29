4f207fcbb8a06f7a43c1421f194a29ef59aa580e
Chad Towle <chad@towlebooth.com>
2019-01-29 15:43:18 -0600

    Added first routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import AppNavbar from './components/AppNavbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//import AppNavbar from './components/AppNavbar';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 import MovieList from './components/MovieList';
 import MovieModal from './components/MovieModal';
 import { Container } from 'reactstrap';
@@ -13,14 +21,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      
       <Provider store={store}>
-        <div className="App">
-          <AppNavbar />
-          <Container>
-            <MovieModal />
-            <MovieList />
-          </Container>
-        </div>
+        <Router>
+          <div className="App">
+              <Navbar />
+              <Route exact path="/" component={Landing} />
+              <div className="container">
+                <Route exact path="/register" component={ Register } />
+                <Route exact path="/login" component={ Login } />
+              </div>
+              <Container>
+                <MovieModal />
+                <MovieList />
+              </Container>
+              <Footer />
+          </div>
+        </Router>
       </Provider>
     );
   }
