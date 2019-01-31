dd78d261cb3f2942593fa365befc948ec5b05143
Chad Towle <chad@towlebooth.com>
2019-01-30 20:00:57 -0600

    Started using redux

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,5 @@ export const GET_MOVIES = 'GET_MOVIES';
 export const ADD_MOVIE = 'ADD_MOVIE';
 export const DELETE_MOVIE = 'DELETE_MOVIE';
 export const MOVIES_LOADING = 'MOVIES_LOADING';
+
+export const TEST_DISPATCH = 'TEST_DISPATCH';
