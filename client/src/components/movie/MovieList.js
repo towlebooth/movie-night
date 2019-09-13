279178873f34130a143e15e74adb64508a23a112
Chad Towle <chad@towlebooth.com>
2019-09-13 14:06:36 -0500

    Added date watched to all movies list

--- client/src/components/movie/MovieList.js
+++ client/src/components/movie/MovieList.js
@@ -5,11 +5,13 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { getMovies, deleteMovie } from '../../actions/movieActions';
+import { getMovieNights } from '../../actions/movieNightActions';
 import PropTypes from 'prop-types';
 
 class MovieList extends Component {
     componentDidMount() {
         this.props.getMovies();
+        this.props.getMovieNights();
     }
 
     onDeleteClick = (id) => {
@@ -18,15 +20,37 @@ class MovieList extends Component {
 
     render() {
         const { movies } = this.props.movie;
+        const { movieNights } = this.props.movieNight;
+        let moviesForList = [];
+
+        if (movieNights && movies) {
+            movies.forEach((movie) => { 
+                var movieForList = {};
+                movieForList._id = movie._id;
+                movieForList.releaseDate = movie.releaseDate;
+                movieForList.title = movie.title;
+                movieForList.imdbId = movie.imdbId;
+                var i;
+                for (i = 0; i < movieNights.length; i++) { 
+                    if (movieNights[i].movieViewed === movie.imdbId) {
+                        movieForList.dateWatched = movieNights[i].date;
+                        break;
+                    } else {
+                        movieForList.dateWatched = null;
+                    }                    
+                    moviesForList.push(movieForList);
+                }
+            });
+        }
         return(
             <Container>
                 
                 <ListGroup>
                     <TransitionGroup className="movie-list">
-                        {movies.map(({ _id, title, releaseDate, imdbId }) => (
+                        {moviesForList.map(({ _id, title, releaseDate, imdbId, dateWatched }) => (
                             <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem>
-                                    <Link to={`/movie/${imdbId}`}>{title}</Link> ({moment(releaseDate).format('YYYY')})
+                                    <Link to={`/movie/${imdbId}`}>{title}</Link> ({moment(releaseDate).format('YYYY')}) watched <Link to={`/movieNight/${moment.utc(dateWatched).format('YYYY-MM-DD')}`}>{moment.utc(dateWatched).format('YYYY-MM-DD')}</Link>
                                 </ListGroupItem>
                             </CSSTransition>
                         ))}
@@ -38,12 +62,15 @@ class MovieList extends Component {
 }
 
 MovieList.propTypes = {
-    getMovies: PropTypes.func.isRequired, 
-    movie: PropTypes.object.isRequired
+    getMovies: PropTypes.func.isRequired,
+    getMovieNights: PropTypes.func,
+    movie: PropTypes.object.isRequired,    
+    movieNight: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-    movie: state.movie
+    movie: state.movie,
+    movieNight: state.movieNight
 })
 
-export default connect(mapStateToProps, { getMovies, deleteMovie })(MovieList);
+export default connect(mapStateToProps, { getMovies, getMovieNights, deleteMovie })(MovieList);
