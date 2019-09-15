5d03456b88583dd62305d875be9951500c35d9f8
Chad Towle <chad@towlebooth.com>
2019-09-15 17:39:29 -0500

    Simple UI tweaks

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -56,7 +56,7 @@ class Register extends Component {
             <div className="row">
                 <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">Sign Up</h1>
-                <p className="lead text-center">Create your DevConnector account</p>
+                <p className="lead text-center">Create your Movie Night account</p>
                 <form noValidate onSubmit={this.onSubmit}>
                     <TextFieldGroup
                         placeholder="Name"
