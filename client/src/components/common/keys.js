5776641c08d96642d5718b588dfa461516e44a1a
Chad Towle <chad@towlebooth.com>
2019-09-29 17:15:27 -0500

    deleting keys

--- client/src/components/common/keys.js
+++
@@ -1,7 +0,0 @@
-export const WEATHER_API_KEY = "56ae336f89cfc122f6ec1a3780077259";
-
-export const MOVIE_DB_BASE_URL = "https://api.themoviedb.org/3/";
-export const MOVIE_DB_API_KEY = "dce8edd730194999ced0051496bc554e";
-
-export const OMDB_BASE_URL = 'http://www.omdbapi.com/?';
-export const OMDB_API_KEY = '3ff32cc3';
