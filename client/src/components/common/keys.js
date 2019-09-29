9b25f087e1d24d0c0a2cd8a5b4801ede294523d2
Chad Towle <chad@towlebooth.com>
2019-09-29 17:19:20 -0500

    gitignore

--- client/src/components/common/keys.js
+++
@@ -1,5 +0,0 @@
-export const MOVIE_DB_BASE_URL = "https://api.themoviedb.org/3/";
-export const MOVIE_DB_API_KEY = "dce8edd730194999ced0051496bc554e";
-
-export const OMDB_BASE_URL = 'http://www.omdbapi.com/?';
-export const OMDB_API_KEY = '3ff32cc3';
