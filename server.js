e868ff3b5acd7c4c79e850571cffc8cc818219a9
Chad Towle <chad@towlebooth.com>
2019-09-29 17:43:34 -0500

    Prepping for production deploy to Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const movieNights = require('./routes/api/movieNights');
 const movies = require('./routes/api/movies');
@@ -35,6 +36,15 @@ app.use('/api/movieNights', movieNights);
 app.use('/api/movies', movies);
 app.use('/api/users', users);
 app.use('/api/hostingOrders', hostingOrders);
+
+// Serve static assets in Producion
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(espress.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
