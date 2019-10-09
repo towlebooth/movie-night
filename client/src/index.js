45274930ae48585720db5864d7cb36f60e5caa94
Chad Towle <chad@towlebooth.com>
2019-10-09 14:01:09 -0500

    Added react app polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
