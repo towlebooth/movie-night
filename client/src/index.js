c43c280be09e671b17d17662e4cf19c4ef1ccfaa
Chad Towle <chad@towlebooth.com>
2019-10-09 14:37:14 -0500

    Added babel polyfill to try and fix iOS/macOS issue

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+require("babel-polyfill");
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
