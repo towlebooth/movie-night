a3ee6962de6fb5ee87a01e00208a329e78618c49
Chad Towle <chad@towlebooth.com>
2019-11-27 06:48:52 -0600

    Temp fix for incorrect next host bug due to substitution

--- client/src/components/hosting-order/HostingOrder.js
+++ client/src/components/hosting-order/HostingOrder.js
@@ -40,13 +40,15 @@ class HostingOrder extends Component {
             });
 
             // find next host            
-            let mostTimeSinceHosted = movieNightHostingOrders.slice(0, 10);            
-            var hostingOrderByDate = mostTimeSinceHosted.sort(function(a, b) {
+            let hostingOrderCollection = movieNightHostingOrders.slice(0, 10);
+            var hostingOrderByDate = hostingOrderCollection.sort(function(a, b) {
                 return new Date(b.mostRecentDate) - new Date(a.mostRecentDate);
             });
 
-            mostTimeSinceHosted = hostingOrderByDate[hostingOrderByDate.length - 1];
-            if (mostTimeSinceHosted) {
+            //let mostTimeSinceHosted = hostingOrderByDate[hostingOrderByDate.length - 1];
+            let mostTimeSinceHosted = hostingOrderByDate[6];
+            if (mostTimeSinceHosted) {                
+                console.log("mostTimeSinceHosted: " + mostTimeSinceHosted.host)
                 movieNightHostingOrders.forEach((hostingOrderMovieNight) => {                    
                         if (hostingOrderMovieNight._id === mostTimeSinceHosted._id) {
                             hostingOrderMovieNight.isNext = true;
