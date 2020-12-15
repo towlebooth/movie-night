f097c0eba6be0b341ecb3633f05c438336e3a76c
Chad Towle <chad@towlebooth.com>
2020-12-15 11:28:44 -0600

    Updating mongo key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://movieApp:Redav521@ds058739.mlab.com:58739/movie_night',
+    //mongoURI: 'mongodb://movieApp:Redav521@ds058739.mlab.com:58739/movie_night',
+    mongoURI: 'mongodb+srv://movieApp:Redav53!@movie-night.pnzdh.mongodb.net/movie_night?retryWrites=true&w=majority',
     secretOrKey: 'secret'
 }
